7b8225e76614dbb5d8212d4021b89a6256104f25
AkshayRaul <raulakshay@gmail.com>
2018-10-13 10:35:47 -0400

    Tried Classifier

--- google_search.py
+++ google_search.py
@@ -8,7 +8,7 @@ import pandas as pd
 import relevance
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB,MultinomialNB
 from sklearn import metrics
 
 
@@ -16,6 +16,7 @@ def getDocuments(start,end):
     output = open("results.csv", "w+")
     i = start
     save_documents = np.array([])
+    target_data=[]
     while (i < end):
 
         search_query = list.loc[i][0]
@@ -99,49 +100,61 @@ def getDocuments(start,end):
         save_documents=np.append(save_documents,ans)
         match = relevance.token(ans)
         if match > 0.25:
+            target_data.append(1)
             output.write(search_query + "," + str(match) + "," + target_url + ",LEGAL\n")
         else:
+            target_data.append(0)
             output.write(search_query + "," + str(match) + ",N/A,NOT LEGAL\n")
         i += 1
     output.close()
-    return save_documents
+    return save_documents, target_data
 
-target_set=[1,0,0,1,0,1,1,1,0,1]#,0,0,1,1,1,0,1,0,1,1]
-test_target=[1,0,1,0,0,1,0,1,1,0]#1,0,0,1,1,1,0,1,0,1,1]
-# target_set=[0,0]
-# test_target=[1,1]
-print(len(target_set))
+# target_set=np.array([1,0,0,1,1,1,1,1,1,1])#,0,0,1,1,1,0,1,0,1,1]
+# test_target=np.array([1,0,1,0,0])#1,0,0,1,1,1,0,1,0,1,1]
+# # target_set=[0,0]
+# # test_target=[1,1]
+# print(len(target_set))
 list=pd.read_csv("Bad_Citation_Searches.csv",delimiter=",", encoding="UTF-8", error_bad_lines=False)
-
-
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(getDocuments(0,10))
-X_train_counts.shape
-
-
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-X_train_tfidf.shape
-print(X_train_tfidf.shape)
-
-clf = MultinomialNB().fit(X_train_tfidf, target_set)
-
-#testing
-test_documents=getDocuments(10,20)
-print(len(test_documents))
-X_test_counts=count_vect.transform(test_documents)
-X_test_tfidf=tfidf_transformer.transform(X_test_counts)
-
-predicted=clf.predict(X_test_tfidf)
-for a,b in zip(test_documents,predicted):
-    print(target_set[b])
-
-print(metrics.classification_report(test_target, predicted,target_names=["LEGAL","NOT LEGAL"]))
-
-
-
-
-
+#
+#
+# count_vect = CountVectorizer()
+# X_train_counts = count_vect.fit_transform(getDocuments(0,10))
+# X_train_counts.shape
+#
+#
+# tfidf_transformer = TfidfTransformer()
+# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+# X_train_tfidf.shape
+# print(X_train_tfidf.shape)
+#
+# clf = MultinomialNB().fit(X_train_tfidf, target_set)
+#
+# print("Starting test data analysis")
+# #testing
+# test_documents=getDocuments(10,15)
+# print(len(test_documents))
+#
+# predicted=clf.predict(test_documents)
+# mean=np.mean(predicted == test_target)
+# print(predicted)
+#
+# #
+# # X_test_counts=count_vect.transform(test_documents)
+# # X_test_tfidf=tfidf_transformer.transform(X_test_counts)
+# #
+# # print("End of tfidf")
+# # print(X_test_tfidf)
+# #
+# # predicted=clf.predict(X_test_tfidf)
+# # for a,b in zip(test_documents,predicted):
+# #     print(target_set[b])
+#
+# print(metrics.classification_report(test_target, predicted,target_names=["LEGAL","NOT LEGAL"]))
+#
+#
+#
+#
+#
 
 
 
