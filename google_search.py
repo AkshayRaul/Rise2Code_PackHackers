1c490eb5bc913aeaf4672a67a57186f0640d67cd
ravisanker <ravisanker93@gmail.com>
2018-10-12 23:35:17 -0400

    Graceful request handling

--- google_search.py
+++ google_search.py
@@ -13,33 +13,50 @@ list=pd.read_csv("Bad_Citation_Searches.csv",delimiter=",", encoding="UTF-8", er
 print(list)
 for i in range(5):
     search_query = list.loc[i][0]
-    url =  "http://www.google.com/search?q=" + parse.quote(search_query)
+    url = "http://www.google.com/search?q=" + parse.quote(search_query)
     url_pool_manager = urllib3.PoolManager()
-    response = url_pool_manager.request("GET", url)
-    soup = BeautifulSoup(response.data,features='lxml')
+    search_response = url_pool_manager.request("GET", url)
+    search_response_DOM = BeautifulSoup(search_response.data,features='lxml')
     #print(soup)
-    soup_ids=soup.find_all("cite")
+    cite_ids=search_response_DOM.find_all("cite")
 
 
     #print(soup_ids)
 
-    # SKIP PDF FILES
-    for soup_id in soup_ids:
-        abc = str(soup_id.decode_contents()).split(".")[-1]
-        print(abc)
-        if abc != "pdf":
+    # Skip PDF files and select first non-pdf file in results page.
+    for cite_id in cite_ids:
+        last_segment = str(cite_id.decode_contents()).split(".")[-1]
+        #print(segment)
+        if last_segment != "pdf":
             break
 
     # print(soup_id)
-    soup_id=soup_id.parent
-    soup_id = soup_id.parent.parent
-    soup_array = soup_id.find('a')['href']
-    target_url = soup_array.split('=')[1]
+    # get the anchor
+    if cite_id is None:
+        output.write(search_query + "," + str(match) + ",N/A,NOT LEGAL\n")
+        continue
+
+    anchor_element_cite_id=cite_id.parent.parent.parent
+    if anchor_element_cite_id is None:
+        output.write(search_query + "," + str(match) + ",N/A,NOT LEGAL\n")
+        continue
+
+    anchor_url = anchor_element_cite_id.find('a')['href']
+    target_url = anchor_url.split('=')[1]
     target_url = target_url.split('&')[0]
     print(target_url)
 
     # call target url
-    response = url_pool_manager.request("GET", target_url)
+    try:
+        response = url_pool_manager.request("GET", target_url)
+    except requests.exceptions.ConnectionError:
+        output.write(search_query + "," + str(match) + ",N/A,NOT LEGAL\n")
+        continue
+
+    if response.status != 200:
+        output.write(search_query + "," + str(match) + ",N/A,NOT LEGAL\n")
+        continue
+
     bodysoup = BeautifulSoup(response.data,'lxml')
 
     data = bodysoup.findAll(text=True)
@@ -64,7 +81,7 @@ for i in range(5):
     if match>0.25:
         output.write(search_query+","+str(match)+","+target_url+",LEGAL\n")
     else: output.write(search_query+","+str(match)+",N/A,NOT LEGAL\n")
-output.close()
+    output.close()
     #WORKING CODE FOR ENTIRE TEXT
     # file_object = open(r"sample.txt","w")
     # file_object.write(ans.encode('utf-8'))
